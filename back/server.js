dc5587e61c79c87706baff1f7b2e06dc14fd2c80
mariahedkvist <mariasofiahedkvist@gmail.com>
2024-01-26 13:21:14 +0100

    Complete CRUD functionality

--- back/server.js
+++ back/server.js
@@ -12,12 +12,26 @@ app.use(express.json());
 connect(); // Connect to MongoDB
 
 // CRUD
+// TODO: generalisera felhantering
 app.get('/api/v1/entries', async (req, res) => {
   try {
     const entries = await Entry.find();
     res.status(200).json({ success: true, data: entries });
   } catch (err) {
     res.status(400).json({ success: false, error: err }); // TODO: korrekta felmedd
+  }
+});
+
+app.get('/api/v1/entries/:id', async (req, res) => {
+  try {
+    const param = req.params.id;
+    const entry = await Entry.findById(param);
+
+    if (!entry) return res.status(404).json({ success: false });
+
+    res.status(200).json({ success: true, data: entry });
+  } catch (err) {
+    res.status(400).json({ success: false, error: err.message }); // TODO: korrekta felmedd
   }
 });
 
@@ -35,6 +49,31 @@ app.post('/api/v1/entries', async (req, res) => {
     } else {
       res.status(400).json({ success: false, error: err.message }); // TODO: korrekta felmedd
     }
+  }
+});
+
+app.put('/api/v1/entries/:id', async (req, res) => {
+  try {
+    const entry = await Entry.findByIdAndUpdate(req.params.id, req.body);
+
+    if (!entry) return res.status(404).json({ success: false });
+
+    res.status(204).json({ success: true, data: entry });
+    // res.status(204).send();
+  } catch (err) {
+    res.status(400).json({ success: false, error: err.message }); // TODO: korrekta felmedd
+  }
+});
+
+app.delete('/api/v1/entries/:id', async (req, res) => {
+  try {
+    const entry = await Entry.findByIdAndDelete(req.params.id);
+
+    if (!entry) return res.status(404).json({ success: false });
+
+    res.status(204).json({ success: true, data: entry });
+  } catch (err) {
+    res.status(400).json({ success: false, error: err.message }); // TODO: korrekta felmedd
   }
 });
 
